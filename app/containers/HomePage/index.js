d94f28d2bde2aa7d867b11996f9832b7f1867d05
kulek1 <mati.798@hotmail.com>
2019-05-30 12:43:12 +0200

    Disable rules: no-extraneous-dependencies, jsx-a11y/label-has-associated-control

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,26 +1,20 @@
 // @flow
-import React, { Component } from 'react';
+import React from 'react';
 import ActiveListeners from '../../components/ActiveListeners';
 import ServerStatus from '../../components/ServerStatus';
 import AudioSelect from '../../components/AudioSelect';
 
-class HomePage extends Component<Props> {
-  props: Props;
-
-  render() {
-    return (
-      <div className="container">
-        <h3 className="app-title">Streaming-Server</h3>
-        <div className="container__boxes">
-          <ActiveListeners />
-          <ServerStatus />
-        </div>
-        <div className="container__select">
-          <AudioSelect />
-        </div>
-      </div>
-    );
-  }
-}
+const HomePage = () => (
+  <div className="container">
+    <h3 className="app-title">Streaming-Server</h3>
+    <div className="container__boxes">
+      <ActiveListeners />
+      <ServerStatus />
+    </div>
+    <div className="container__select">
+      <AudioSelect />
+    </div>
+  </div>
+);
 
 export default HomePage;
