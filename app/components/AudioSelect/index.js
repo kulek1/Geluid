9260912793ac12334ef978132834711db957ca6e
kulek1 <mati.798@hotmail.com>
2019-07-08 20:40:44 +0200

    Replace class component to hooks

--- app/components/AudioSelect/index.js
+++ app/components/AudioSelect/index.js
@@ -1,14 +1,10 @@
 // @flow
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Select from 'react-select';
 import { ipcRenderer } from 'electron';
 import styles from './styles.scss';
 import customSelectStyle from './StyledSelect';
-
-type Props = {
-  audioDevices: array
-};
 
 const placeholder = (
   <span>
@@ -17,48 +13,38 @@ const placeholder = (
   </span>
 );
 
-class AudioSelect extends Component<Props> {
-  props: Props;
+const AudioSelect = ({ audioDevices }: Props) => {
+  const [selectedOption, setSelectedOption] = useState('');
 
-  state = {
-    selectedOption: ''
-  };
-
-  updateValue = value => {
-    this.setState({
-      selectedOption: value
-    });
-    this.sendSelectedAudioDeviceId(value.value);
-  };
-
-  sendSelectedAudioDeviceId = deviceId =>
+  const sendSelectedAudioDeviceId = deviceId =>
     ipcRenderer.send('client-audio-devices', deviceId);
 
-  render() {
-    const { audioDevices } = this.props;
-    const { selectedOption } = this.state;
-    const devices = audioDevices.map(item => ({
-      label: item.name,
-      value: item.id
-    }));
-
-    return (
-      <div className={styles.container}>
-        <h5 className={styles.heading}>Select audio input</h5>
-        <Select
-          isLoading={audioDevices.length === 0}
-          name="form-field-name"
-          value={selectedOption}
-          options={devices}
-          onChange={this.updateValue}
-          styles={customSelectStyle}
-          maxMenuHeight={150}
-          placeholder={placeholder}
-        />
-      </div>
-    );
+  function updateValue(value) {
+    setSelectedOption(value);
+    sendSelectedAudioDeviceId(value.value);
   }
-}
+
+  const devices = audioDevices.map(item => ({
+    label: item.name,
+    value: item.id
+  }));
+
+  return (
+    <div className={styles.container}>
+      <h5 className={styles.heading}>Select audio input</h5>
+      <Select
+        isLoading={audioDevices.length === 0}
+        name="form-field-name"
+        value={selectedOption}
+        options={devices}
+        onChange={updateValue}
+        styles={customSelectStyle}
+        maxMenuHeight={150}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = ({ server }) => ({
   audioDevices: server.audioDevices,
